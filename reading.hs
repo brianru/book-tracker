f1457c2c8f052e234e43a72180fc6df4a785cd77
brianru <brianrubinton@gmail.com>
2014-06-01 23:51:38 -0400

    importing shelfari dump

--- reading.hs
+++ reading.hs
@@ -3,26 +3,42 @@ import System.Locale
 import Data.List.Split
 import System.IO
 
-data Book = Book { title :: String
+data Book = Book { isbn :: Int
+                 , title :: String
                  , author :: String
-                 , whenRead :: [Day]
-                 , pages :: Integer
-                 , words :: Integer
-                 , pred :: Book
-                 , succ :: Book
+                 , whenRead :: [String]  -- date type
+                 , whenAdded :: String -- date type
+                 , pageCount :: Integer -- Maybe
+                 , wordCount :: Integer -- Maybe
+                 --, pred :: Book
+                 --, succ :: Book
                  } deriving (Show, Eq)
 
-parseDay :: String -> Day
-parseDay s = readTime defaultTimeLocale "%-m%-d%Y" s
-
 bookFromShelfariEntry :: [String] -> Book
-bookFromShelfariEntry x = Book {title = x!!3,
-                                author = x!!4,
-                                whenRead = [(parseDay $ x!!14)] }
+-- fill in data from api
+bookFromShelfariEntry x = Book { isbn = x!!2
+                               , title = x!!3
+                               , author = x!!4
+                               , whenRead = [x!!14]
+                               , whenAdded = x!!20
+                               , pageCount = 0
+                               , wordCount = 0
+                               }
+
+readShelfariData :: String -> [Book]
+readShelfariData f = let (x:xs) = map (splitOn "\t") $ lines f
+                     in map bookFromShelfariEntry xs
+
+importShelfari :: FilePath -> FilePath -> IO ()
+importShelfari fin fout = do tsv <- readFile fin
+                             let books = readShelfariData tsv
+                             writeFile fout $ show books
 
 
-importShelfari :: FilePath -> [Book]
-importShelfari file = do tsv <- readFile file
-                         let (x:xs) = map (splitOn "\t") lines tsv
-                             books = map bookFromShelfariEntry xs
-                         putStrLn(books)
+-- TODO
+-- persistence
+-- connect with api
+-- give me a way to update a book
+-- statuses
+-- load/save
+-- stats
